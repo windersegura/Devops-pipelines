2fd657dba195ecb57ce0607bf545bb971d3716a9
winders98 <segurawinder@gmail.com>
2026-07-15 11:43:43 -0600

    feat: add github pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hi Devops")
+console.log("I'm learning CI/CD with Github Actions")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting tests....")
+setTimeout(() => console.log("Waiting 3 minutes"),3000)
+console.log("Ending tests.....")
